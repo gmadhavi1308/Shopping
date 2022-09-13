0b0bac7ccbea135ee824dc49d59e485ad34de87f
Sai Sravani <101802224+gmadhavi1308@users.noreply.github.com>
2022-09-13 21:17:54 +0530

    Add files via upload

---
+++ OnlineShopping/shop/urls.py
@@ -0,0 +1,34 @@
+"""OnlineShopping URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("cart/", views.cart, name="cart"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("update_item/", views.updateItem, name="update_item"),
+    path("product_view/<int:myid>/", views.product_view, name="product_view"),
+    path("search/", views.search, name="search"),
+    path("tracker/", views.tracker, name="tracker"),
+    path("contact/", views.contact, name="contact"),
+    path("loggedin_contact/", views.loggedin_contact, name="loggedin_contact"),
+
+    path("register/", views.register, name="register"),
+    path("change_password/", views.change_password, name="change_password"),
+    path("login/", views.Login, name="login"),
+    path("logout/", views.Logout, name="logout"),
+]
